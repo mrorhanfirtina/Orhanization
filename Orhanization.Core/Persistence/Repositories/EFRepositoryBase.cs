4749728532823cbd5602157fb735b2aba14bdb57
bay-lavabo <85457462+bay-lavabo@users.noreply.github.com>
2024-07-30 11:16:03 +0300

    GetListNoPaginate Added

--- Orhanization.Core/Persistence/Repositories/EFRepositoryBase.cs
+++ Orhanization.Core/Persistence/Repositories/EFRepositoryBase.cs
@@ -397,6 +397,43 @@ public class EFRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
         return queryable.ToPaginate(index, size, cancellationToken);
     }
 
+    public IQueryable<TEntity> GetListNoPaginate(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, bool withDeleted = false, bool enableTracking = true, bool autoInclude = false, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> queryable = Query();
+        if (!enableTracking)
+        {
+            queryable = queryable.AsNoTracking();
+        }
+
+        if (include != null)
+        {
+            queryable = include(queryable);
+        }
+
+        if (withDeleted)
+        {
+            queryable = queryable.IgnoreQueryFilters();
+        }
+
+        if (!autoInclude)
+        {
+            queryable.IgnoreAutoIncludes();
+        }
+
+        if (predicate != null)
+        {
+            queryable = queryable.Where(predicate);
+        }
+
+        if (orderBy != null)
+        {
+            return orderBy(queryable);
+        }
+
+        return queryable;
+    }
+
+
     public Paginate<TEntity> GetListByDynamic(DynamicQuery dynamic, Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, bool autoInclude = false, CancellationToken cancellationToken = default)
     {
         IQueryable<TEntity> queryable = Query().ToDynamic(dynamic);
