95a685a368b716a61745c845e7e2437bdf6f6c79
bay-lavabo <85457462+bay-lavabo@users.noreply.github.com>
2024-08-19 15:17:09 +0300

    fix

---
+++ Orhanization.Core/Application/Events/TransactionScopeEvents.cs
@@ -0,0 +1,14 @@
+﻿namespace Orhanization.Core.Application.Events;
+
+public static class TransactionScopeEvents
+{
+    public static event Func<Task> OnTransactionCompleted;
+
+    public static async Task RaiseTransactionCompletedEvent()
+    {
+        if (OnTransactionCompleted != null)
+        {
+            await OnTransactionCompleted.Invoke();
+        }
+    }
+}
